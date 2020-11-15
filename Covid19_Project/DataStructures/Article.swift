32fa7bc6ca260f9d496831c877a2fb3f8375467f
Yauheni Hramiashkevich <yauheni.hramiashkevich@apalon.com>
2020-11-15 11:51:12 +0300

    Added phot background

--- Covid19_Project/DataStructures/Article.swift
+++ Covid19_Project/DataStructures/Article.swift
@@ -1,5 +1,19 @@
 
-struct Article {
-    let author: String
-    let title: String
+struct News: Codable {
+    let status: String
+    let totalResults: Int
+    let articles: [Article]
 }
+
+// MARK: - Article
+struct Article: Codable {
+    let author: String?
+    let title: String
+    let url: String?
+    let urlToImage: String?
+    let content: String?
+
+
+}
+
+
--- Covid19_Project/ViewControllers/NewsViewController.swift
+++ Covid19_Project/ViewControllers/NewsViewController.swift
@@ -6,47 +6,91 @@
 //
 
 import UIKit
+import SafariServices
+
 
 class NewsViewController: UIViewController {
-    @IBOutlet weak var collectionView: UICollectionView!
     
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var titleLabelInCell: UILabel!
     @IBOutlet weak var authorLabelInCell: UILabel!
+   
+
     
-    let initialNews: [Article] = [
-        Article(author: "Me", title: "Breaking News"),
-        Article(author: "He", title: "Smth"),
-        Article(author: "She", title: "Another one"),
-        Article(author: "Me", title: "Breaking News"),
-       
-    ]
-    
-    
-    
+    var newsApiURLString = "https://newsapi.org/v2/top-headlines?country=us&apiKey=a05c63a1c38c497babb576e49676a0d1&category=health"
+    var newses: News?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.topItem?.title = "News"
         collectionView.dataSource = self
+        getNewses()
+    }
+    
+    func getNewses()  {
+        let urlSessin = URLSession(configuration: .default)
+        guard let newsApiUrl = URL(string: newsApiURLString) else {return}
+        let request = URLRequest(url: newsApiUrl)
+       let dataTask =  urlSessin.dataTask(with: request) { (data, response, error) in
+        if error != nil {
+                print(error)
+            }
+        
+        if let data = data {
+            do {
+                self.newses = try JSONDecoder().decode(News.self, from: data)
+                print(self.newses?.articles.count)
+                DispatchQueue.main.async { [self] in
+                    self.collectionView.reloadData()
+                }
+            }
+            catch {
+                print(error)
+            }
+        }
+        }
+        dataTask.resume()
+    }
+    
+    func openNewsInSafari(_ which: Int) {
+        if let url = URL(string: newses?.articles[which].url ?? "https://www.raywenderlich.com/5293-operation-and-operationqueue-tutorial-in-swift") {
+            let config = SFSafariViewController.Configuration()
+            config.entersReaderIfAvailable = true
+
+            let vc = SFSafariViewController(url: url, configuration: config)
+            present(vc, animated: true)
+        }
     }
 }
 
-extension NewsViewController: UICollectionViewDataSource {
+extension NewsViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return initialNews.count
+        return newses?.articles.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "News", for: indexPath) as! NewsCell
-        cell.titleLabel.text = initialNews[indexPath.row].title
-        cell.authorLabel.text = initialNews[indexPath.row].author
+        cell.titleLabel.text = newses?.articles[indexPath.row].title
+        cell.authorLabel.text = newses?.articles[indexPath.row].author
+        
+        guard let imageURL = URL(string:newses?.articles[indexPath.row].urlToImage ?? "http://www.raywenderlich.com/downloads/ClassicPhotosDictionary.plist"  ),
+          let imageData = try? Data(contentsOf: imageURL) else {
+            return cell
+        }
 
+        //1
+        let unfilteredImage = UIImage(data:imageData)
+        //2
+        cell.backgroundColor = UIColor(patternImage: (unfilteredImage ?? UIImage(named: "DefaultCellBackground"))!)
+        
         cell.layer.borderWidth = 5
         cell.layer.masksToBounds = true
         cell.layer.cornerRadius = 5
-           return cell
+        return cell
     }
     
-    
-    
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at:indexPath) as! UICollectionViewCell
+        print("Print")
+    }
 }
