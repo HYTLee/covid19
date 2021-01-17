05978935cf010f228207e339f00625d23bbcb0c5
Yauheni Hramiashkevich <yauheni.hramiashkevich@apalon.com>
2021-01-17 14:24:04 +0300

    refactor news cell

--- Covid19_Project/ViewControllers/News/NewsCell.swift
+++ Covid19_Project/ViewControllers/News/NewsCell.swift
@@ -13,7 +13,6 @@ class NewsCell: UICollectionViewCell {
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
 
-    
     var imageIV = NewsImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
     
     
@@ -21,15 +20,11 @@ class NewsCell: UICollectionViewCell {
        super.awakeFromNib()
         backgroundView = UIView()
         backgroundView?.addSubview(imageIV)
-    }
-    
-    override func setNeedsLayout() {
         layer.borderWidth = 5
         layer.masksToBounds = true
         layer.cornerRadius = 5
-       
     }
-    
+ 
     override func prepareForReuse() {
         super.prepareForReuse()
     }
