845be5e201a720257a3a6e188ca7f7ee65fba682
Yauheni Hramiashkevich <yauheni.hramiashkevich@apalon.com>
2021-02-09 19:48:55 +0300

    add git ignore

--- Pods/Target Support Files/Pods-Covid19_Project-Covid19_ProjectUITests/Pods-Covid19_Project-Covid19_ProjectUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Covid19_Project_Covid19_ProjectUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Covid19_Project_Covid19_ProjectUITests
-@end
